6c2705c861ed51682ced88e0d8cf8966dd67a7c6
Stefan Mesken <KappaDistributive@users.noreply.github.com>
2018-12-06 12:14:57 +0100

    solved day 2

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/Users/stefan/Documents/Programming/Rust/aoc2018rust/day2/target/debug/day2.dSYM: /Users/stefan/Documents/Programming/Rust/aoc2018rust/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-aa7c9ade09133789.d
@@ -0,0 +1,5 @@
+/Users/stefan/Documents/Programming/Rust/aoc2018rust/day2/target/debug/deps/day2-aa7c9ade09133789: src/main.rs
+
+/Users/stefan/Documents/Programming/Rust/aoc2018rust/day2/target/debug/deps/day2-aa7c9ade09133789.d: src/main.rs
+
+src/main.rs:

f931abaa52480f5c023a0b718b23196149ce90a6
Stefan Mesken <stefan@Germain.Speedport_W_724V_Typ_A_05011603_06_001>
2018-12-07 13:10:35 +0100

    removed target

--- day6/target/debug/day6-8bb9ba073cf1f96c.d
+++
@@ -1,1 +0,0 @@
-/home/stefan/Documents/Programming/Rust/aoc2018rust/day6/target/debug/day6-8bb9ba073cf1f96c: /home/stefan/Documents/Programming/Rust/aoc2018rust/day6/src/main.rs
--- day6/target/debug/deps/day6-8bb9ba073cf1f96c.d
+++
@@ -1,5 +0,0 @@
-/home/stefan/Documents/Programming/Rust/aoc2018rust/day6/target/debug/deps/day6-8bb9ba073cf1f96c: src/main.rs
-
-/home/stefan/Documents/Programming/Rust/aoc2018rust/day6/target/debug/deps/day6-8bb9ba073cf1f96c.d: src/main.rs
-
-src/main.rs:

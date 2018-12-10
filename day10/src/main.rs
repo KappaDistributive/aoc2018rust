e521163fde27adba456840515e6850f935b33aeb
Stefan Mesken <KappaDistributive@users.noreply.github.com>
2018-12-10 19:06:40 +0100

    solved part 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,60 +3,144 @@ extern crate regex;
 extern crate lazy_static;
 use std::collections::HashMap;
 use std::cmp;
+use std::ops;
 use std::i32;
 
 use regex::Regex;
 
-const INPUT: &str = include_str!("../input.txt");
-
-type Point = (i32,i32);
-
-fn format_input(input_str: &str) -> HashMap<Point,Point> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"[^0-9-]*(-?[0-9]+)[^0-9-]*(-?[0-9]+)[^0-9-]*(-?[0-9]+)[^0-9-]*(-?[0-9])[^0-9-]*").unwrap();
-    }
-    let mut result: HashMap<Point,Point> = HashMap::new();
-    for line in input_str.trim().lines() {
-        let mut entry: (Point,Point) = ((0,0),(0,0));
-        match RE.captures(line) {
-            Some(caps) => {
-                entry.0 = (caps.get(1).map_or(0i32, |m| m.as_str().parse::<i32>().unwrap()),
-                            caps.get(2).map_or(0i32, |m| m.as_str().parse::<i32>().unwrap()));
-                entry.1 = (caps.get(3).map_or(0i32, |m| m.as_str().parse::<i32>().unwrap()),
-                           caps.get(4).map_or(0i32, |m| m.as_str().parse::<i32>().unwrap()));
-            }
-            None => {
-                panic!("Couldn't parse: {}", "bla");
-            }
-        }
-        result.insert(entry.0, entry.1);
-    }
-    result
+#[derive(Clone, Copy, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-fn print_points(input: &HashMap<Point,Point>) {
-    let mut min: Point = (-10,-10);
-    let mut max: Point = (20,15);
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
 
-    for y in min.1..=max.1 {
-        for x in min.0..=max.0 {
-            match input.get(&(x,y)) {
-                Some(_) => {
-                    print!("x");
-                }
-                None => {
-                    print!(".");
-                }
-            }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Vector {
+    dx: i32,
+    dy: i32,
+}
+
+impl Vector {
+    fn new(dx: i32, dy: i32) -> Self {
+        Vector { dx, dy}
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Light {
+    pos: Point,
+    vel: Vector,
+}
+
+impl Light {
+    fn step(&mut self) {
+        self.pos += self.vel;
+    }
+}
+
+impl ops::Add<Vector> for Point {
+    type Output = Point;
+    
+    fn add(self, v: Vector) -> Self::Output {
+        Point {
+            x: self.x + v.dx,
+            y: self.y + v.dy,
         }
-        println!("");
+    }
+
+}
+
+impl ops::AddAssign<Vector> for Point {
+    fn add_assign(&mut self, v: Vector) {
+        *self = *self + v;
+    }
+}
+
+const INPUT: &str = include_str!("../input.txt");
+
+
+
+fn format_input(input_str: &str) -> Vec<Light> {
+    let mut lights: Vec<Light> = Vec::new();
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(-?[0-9]+)").unwrap();
+    }
+
+    for line in input_str.trim().lines() {
+        let mut nums: Vec<i32> = Vec::new();
+        for c in RE.captures_iter(line) {
+            nums.push(c[0].parse::<i32>().unwrap());
+        }
+        if nums.len() != 4 {
+            panic!("Couldn't parse: {}", line);
+        }
+        let pos = Point::new(nums[0], nums[1]);
+        let vel = Vector::new(nums[2], nums[3]);
+        lights.push(Light{ pos, vel });
+    }
+    lights
+}
+
+fn border_lights(lights: &Vec<Light>) -> (i32,i32,i32,i32) {
+    let upper = lights.iter().map(|p| p.pos.y).min().unwrap();
+    let lower = lights.iter().map(|p| p.pos.y).max().unwrap();
+    let left = lights.iter().map(|p| p.pos.x).min().unwrap();
+    let right = lights.iter().map(|p| p.pos.x).max().unwrap();
+
+    (upper, lower, left, right)
+}
+
+fn print_lights(lights: &Vec<Light>) {
+    let (upper,lower,left,right) = border_lights(lights);
+    let height = (lower - upper) as usize + 1;
+    let width = (right - left) as usize + 1;
+    let mut state = vec![vec![false;width];height];
+
+    for (x,y) in lights.iter()
+        .map(|l| ((l.pos.x - left) as usize, (l.pos.y - upper) as usize)) {
+        state[y][x] = true;
+    }
+
+    for line in state.iter().map(|row| {
+        row.iter().map(|p| if *p { "#"} else { "." }).collect::<String>()
+    }) {
+        println!("{}", line);
     }
 }
 
 fn solve_part_1(input_str: &str) {
-    let mut input: HashMap<Point,Point> = format_input(input_str);
-    print_points(&input);
+    let mut lights: Vec<Light> = format_input(INPUT);
+    let limit = 20000;
+    let mut i = 1;
+    let b: (i32,i32,i32,i32) = border_lights(&lights);
+    let (upper,lower,left,right) = b;
+    let mut benchmark = (lower - upper) + (right - left);
+    for _ in 0..limit {
+        i += 1;
+        lights.iter_mut().for_each(Light::step);
+        let (t_upper, t_lower, t_left, t_right) = border_lights(&lights);
+        if (t_lower - t_upper) + (t_right - t_left) < benchmark {
+            benchmark = (t_lower - t_upper) + (t_right - t_left) + 100;
+        }
+        else {
+            break;
+        }
+        if benchmark < 200 {
+                print_lights(&lights);
+            println!("i = {}", i);
+        }
+    }
+
 }
+
+
 
 fn main() {
     solve_part_1(INPUT);

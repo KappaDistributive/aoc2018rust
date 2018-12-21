f9c18fa00cabda5830476f10ccf533f6358d7156
Stefan Mesken <stefan@ride.speedport_w_724v_typ_a_05011603_06_001>
2018-12-21 19:16:00 +0100

    solved day 13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,15 +1,224 @@
 use std::cmp::Ordering;
+
 const INPUT_SAMPLE: &str = include_str!("../input_sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 type Point = (usize,usize);
 
-#[derive(Eq)]
+#[derive(Eq,Clone)]
 struct Cart {
     pos: Point,
     direction: char,
     intersection_counter: usize,
 }
+
+impl Cart {
+    fn step(&mut self, track: &Vec<char>, width: usize) -> Self {
+        let mut pos: Point = (0,0);
+        let mut direction: char = ' ';
+        let mut intersection_counter: usize = 0;
+        match self.direction {
+            '^' => {
+                pos = (self.pos.0, self.pos.1 - 1);
+                direction = self.update_direction(track, width);
+                intersection_counter = self.update_intersection_counter(track, width);
+            }
+            'v' => {
+                pos = (self.pos.0, self.pos.1 + 1);
+                direction = self.update_direction(track, width);
+                intersection_counter = self.update_intersection_counter(track, width);
+            }
+            '<' => {
+                pos = (self.pos.0 - 1, self.pos.1);
+                direction = self.update_direction(track, width);
+                intersection_counter = self.update_intersection_counter(track, width);
+            }
+            '>' => {
+                pos = (self.pos.0 + 1, self.pos.1);
+                direction = self.update_direction(track, width);
+                intersection_counter = self.update_intersection_counter(track, width);
+            }
+            _ => {
+                panic!("Unable to complete step for cart at ({},{})", self.pos.0, self.pos.1);
+            }
+        }        
+        Cart { pos, direction, intersection_counter }
+    }
+
+    fn update_intersection_counter(&self, track: &Vec<char>, width: usize) -> usize {
+        let mut tile: char = ' ';
+        match self.direction {
+            '^' => {
+                tile = track[(self.pos.1 - 1) * width + self.pos.0];
+            }
+            'v' => {
+                tile = track[(self.pos.1 + 1) * width + self.pos.0];
+            }
+            '<' => {
+                tile = track[self.pos.1 * width + self.pos.0 - 1];
+            }
+            '>' => {
+                tile = track[self.pos.1 * width + self.pos.0 + 1];
+            }
+            _ => {
+                panic!("Couldn't update intersection counter for cart at ({},{})", self.pos.0, self.pos.1);
+            }
+        }
+        if tile == '+' {
+            return self.intersection_counter + 1;
+        }
+        else {
+            return self.intersection_counter;
+        }
+    }
+    
+    fn update_direction(&self, track: &Vec<char>, width: usize) -> char {
+        match self.direction {
+            '^' => {
+                match track[(self.pos.1 - 1) * width + self.pos.0] {
+                    '|' => {
+                        return '^';
+                    }
+                    '/' => {
+                        return '>';
+                    }
+                    '\\' => {
+                        return '<';
+                    }
+                    '+' => {
+                        match  self.intersection_counter % 3 {
+                            0 => {
+                                return '<';
+                            }
+                            1 => {
+                                return '^';
+                            }
+                            2 => {
+                                return '>';
+                            }
+                            _ => {
+                                panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                            }
+                            
+                        }
+                    }
+                    _ => {
+                        panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                    }
+                }
+            }
+
+            'v' => {
+                match track[(self.pos.1 + 1) * width + self.pos.0] {
+                    '|' => {
+                        return 'v';
+                    }
+                    '/' => {
+                        return '<';
+                    }
+                    '\\' => {
+                        return '>';
+                    }
+                    '+' => {
+                        match  self.intersection_counter % 3 {
+                            0 => {
+                                return '>';
+                            }
+                            1 => {
+                                return 'v';
+                            }
+                            2 => {
+                                return '<';
+                            }
+                            _ => {
+                                panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                            }
+                        }
+                    }
+                    _ => {
+                        panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                    }
+                }
+            }
+
+            '<' => {
+                match track[self.pos.1 * width + self.pos.0 - 1] {
+                    '-' => {
+                        return '<';
+                    }
+                    '/' => {
+                        return 'v';
+                    }
+                    '\\' => {
+                        return '^';
+                    }
+                    '+' => {
+                        match  self.intersection_counter % 3 {
+                            0 => {
+                                return 'v';
+                            }
+                            1 => {
+                                return '<';
+                            }
+                            2 => {
+                                return '^';
+                            }
+                            _ => {
+                                panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                            }
+                            
+                        }
+                    }
+                    _ => {
+                        panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                    }
+                }
+            }
+
+            '>' => {
+                match track[self.pos.1 * width + self.pos.0 + 1] {
+                    '-' => {
+                        return '>';
+                    }
+                    '/' => {
+                        return '^';
+                    }
+                    '\\' => {
+                        return 'v';
+                    }
+                    '+' => {
+                        match  self.intersection_counter % 3 {
+                            0 => {
+                                return '^';
+                            }
+                            1 => {
+                                return '>';
+                            }
+                            2 => {
+                                return 'v';
+                            }
+                            _ => {
+                                panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                            }
+                            
+                        }
+                    }
+                    _ => {
+                        panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+                    }
+                }
+            }
+            
+            _ => {
+                panic!("Couldn't update direction for cart at ({},{})", self.pos.0, self.pos.1);
+            }
+        }
+    }
+}
+
+        
+
+
 
 impl Ord for Cart {
     fn cmp(&self, other: &Cart) -> Ordering {
@@ -88,22 +297,54 @@ impl CartAndTrack {
         carts.sort();
         CartAndTrack { track, width, height, carts, tick, crashes }
     }
-
-
-    // implement this
-    fn step(&mut self) {
-        
+    
+    fn occupied (&self, cart: &Cart) -> Option<usize> {
+        for i in 0..self.carts.len() {
+            if cart.pos == self.carts[i].pos {
+                return Some(i);
+            }
+        }
+        return None;
     }
 
-    // fn get_current_track(&self, x: usize, y: usize) -> char {
-    //     self.current_track[y * self.width + x]
-    // }
+    fn step(&mut self) {
+        let mut crashed: Vec<usize> = Vec::new();
+        self.carts.sort();
+        for i in 0..self.carts.len() {                
+            let mut t_cart = self.carts[i].step(&self.track, self.width);
+            match self.occupied(&t_cart) {
+                // carts with index i and j get into a crash
+                Some(j) => {
+                    self.crashes.push(self.carts[j].pos);
+                    if !crashed.contains(&i) {
+                        crashed.push(i);
+                    }
+                    if !crashed.contains(&j) {
+                        crashed.push(j);
+                    }
+                }
+                // cart with index i is free to move
+                None => {
+                    self.carts[i] = t_cart;
+                }
+            }
+        }
+        crashed.sort();
+        crashed.reverse();
+        for i in crashed {
+            self.carts.remove(i);
+        }
+    }
 
     fn print_current_track(&self) {
         let mut current_track: Vec<char> = self.track.clone();
 
         for c in &self.carts {
             current_track[c.pos.1 * self.width + c.pos.0] = c.direction;
+        }
+
+        for c in &self.crashes {
+            current_track[c.1 * self.width + c.0] = 'X';
         }
 
         for y in 0..self.height {
@@ -115,8 +356,26 @@ impl CartAndTrack {
     }
 }
 
+fn solve_part_1(input_str: &str) -> Point {
+    let mut track: CartAndTrack = CartAndTrack::from_str(input_str);
+    while track.crashes.len() == 0 {
+        track.step();
+    }
+    track.crashes[0]
+}
+
+fn solve_part_2(input_str: &str) -> Point {
+    let mut track: CartAndTrack = CartAndTrack::from_str(input_str);
+    while track.carts.len() > 1 {
+        track.step();
+    }
+    track.carts[0].pos
+}
+
 fn main() {
-    let mut test: CartAndTrack = CartAndTrack::from_str(INPUT_SAMPLE);
-    test.print_current_track();
+    let result_1: Point = solve_part_1(INPUT);
+    println!("Answer part 1: ({},{})", result_1.0, result_1.1);
     
+    let result_2: Point = solve_part_2(INPUT);
+    println!("Answer part 2: ({},{})", result_2.0, result_2.1);
 }
